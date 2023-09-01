4c5e655345273246bd30dd8d41a096b5ae891e9c
thomas lahaye <tholahaye@laposte.net>
2023-09-01 17:32:51 +0200

    Vendredi soir
    preprocessing en cours de dev

--- lot2.py
+++ lot2.py
@@ -1,33 +1,51 @@
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.ensemble import IsolationForest
 import pandas as pd 
 
 from sklearn.model_selection import train_test_split
 
-df = pd.read_csv(r"C:\Users\digigi\Downloads\diabete.csv")
 
-'''class ColumnCleaner:
-    def __init__(self, data):
-        self.data = df
+class Preprocessing:
 
-    def drop_empty_columns(self):
-        empty_cols = self.data.columns[self.data.isnull().all()]
-        self.data.drop(empty_cols, axis=1, inplace=True)
-        processor = ColumnCleaner(df)
-        processor.drop_empty_columns()'''
+    def __init__(self, dataframe, model_type, model, split_rate=0.2):
+        self.df = dataframe
+        self.df_original = dataframe
+        self.type = model_type
+        self.model = model
+        self.split_rate = split_rate
 
-df = df.drop('id', axis=1)
-for colonne in df:
-    df = df.dropna()
+        # TODO: Gerer les NaN
+        self.df = self.df.dropna()
 
-LE = LabelEncoder()
+        # TODO: Encodage
+
+        le = LabelEncoder()
+        for column in self.df.column:
+            if str(self.df.dtypes[column]) == 'object':
+                le.fit(self.df[column].unique())
+                self.df[column] = le.transform(self.df[column])
+
+        #TODO: Elimination des outliers
+
+        #TODO: Standardisation
+
+        #TODO: Split du dataset en train/test
+        X_df = self.df.drop(columns=['target'])
+        y_df = self.df['target']
+
+        self.X_train, self.X_test,\
+            self.y_train, self.y_test = train_test_split(X_df, y_df,
+                                                         test_size=self.split_rate,
+                                                         random_state=1996)
+
 
 def outliers(self, contamination=0.05):
-        outlier_detector = IsolationForest(contamination=contamination)
-        outlier_labels = outlier_detector.fit_predict(self.X)
-        self.df = self.df[outlier_labels == 1]
-        # Conservez les données originales
-        self.original_df = self.df.copy()
-        return self.df
+    outlier_detector = IsolationForest(contamination=contamination)
+    outlier_labels = outlier_detector.fit_predict(self.X)
+    self.df = self.df[outlier_labels == 1]
+    # Conservez les données originales
+    self.original_df = self.df.copy()
+    return self.df
 
 
 '''size = df['target'].nunique()
@@ -38,9 +56,3 @@ if size == 2:
 else:
     pass '''
 
-
-
-X = df
-y = df['target']
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=1996)
