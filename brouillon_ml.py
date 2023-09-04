0a6979bae4f29504a400acaba3cb71b6f625405b
gbottaz <mail@mail.fr>
2023-09-04 15:10:05 +0200

    prediction with regression methods

--- brouillon_ml.py
+++ brouillon_ml.py
@@ -1,52 +1,56 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.metrics import confusion_matrix, classification_report
+from sklearn.metrics import classification_report, mean_squared_error, mean_absolute_error, max_error
 
-import constantes
-TARGET = 'target'
+from constantes import STRUCTURE, TARGET
 
 df = pd.read_csv("data/diabete.csv")
-print("dataset:", df.head())
-model_type = "classification"
+model_type = "Classification"
+#df = pd.read_csv("data/vin.csv")
+#model_type = "Regression"
 
+print("dataset:", df.head())
 
 X = df.drop(columns=[TARGET])
 print("features:", X.head())
 y = df[TARGET]
 print("target:", y.head())
 
-if model_type == "classification":
+if model_type == "Classification":
     classes = set(y.unique())
-print("whole dataset classes:", classes)
-# Todo for check imbalanced dataset
+    print("whole dataset classes:", classes)
+    # Todo for check imbalanced dataset
 
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42)
-if model_type == "classification":
+if model_type == "Classification":
     classes_train = set(y_train.unique())
     if classes != classes_train:
         pass
         #TODO error message : "Training data do not have all classes represented, please change your random seed"
-print("train dataset classes:", classes_train)
-# Todo for check imbalanced dataset
+        print("train dataset classes:", classes_train)
+        # Todo for check imbalanced dataset
 
 
-if model_type == "classification":
+if model_type == "Classification":
     tab_eval = pd.DataFrame(columns=["model", "hyperparameters", "fold", "classe", "accuracy", "precision", "recall", "f1-score"])
-if model_type == "regression":
-    tab_eval = pd.DataFrame(columns=["model", "hyperparameters", "fold", "rmse", "mse", "mae"])
+if model_type == "Regression":
+    tab_eval = pd.DataFrame(columns=["model", "hyperparameters", "fold", "rmse", "mae"])
 
-#print(tab_eval.head())
 
 model_name = "Decision_Tree"
-model = constantes.STRUCTURE[model_type][model_name]["model"]
-#hyperParams = STRUCTURE[model_type][model_name]["hyperparameters"]
+# hyperParams = STRUCTURE[model_type][model_name]["hyperparameters"]
 hyperParams = {"criterion": 'gini',
                "max_depth": None,
                "min_samples_split": 2,
                "min_samples_leaf": 1,
                "max_leaf_nodes": None}
+
+#model_name = "Ridge_Regression"
+# hyperParams = STRUCTURE[model_type][model_name]["hyperparameters"]
+#hyperParams = {"alpha": 1}
+
+model = STRUCTURE[model_type][model_name]["model"]
 model.set_params(**hyperParams)
 
 '''
@@ -69,16 +73,15 @@ except:
 
 y_pred = model.predict(X_test)
 
-if model_type == "classification":
+if model_type == "Classification":
     report_dict = classification_report(y_test, y_pred, output_dict=True)
     print(report_dict)
 
     for cl in classes:
         row = {"model": model_name,
                "hyperparameters": hyperParams,
-               "fold": fold_number,
+               #"fold": fold_number,
                "classe": cl,
-               #"accuracy": report_dict[cl]['f1-score'],
                "precision": report_dict[cl]['precision'],
                "recall": report_dict[cl]['recall'],
                "f1-score": report_dict[cl]['f1-score']}
@@ -86,12 +89,21 @@ if model_type == "classification":
 
     row = {"model": model_name,
            "hyperparameters": hyperParams,
-           "fold": fold_number,
+           #"fold": fold_number,
            "classe": "__all (macro avg)",
            "accuracy": report_dict["accuracy"],
            "precision": report_dict['macro avg']['precision'],
            "recall": report_dict['macro avg']['recall'],
            "f1-score": report_dict['macro avg']['f1-score']}
+    tab_eval = pd.concat([tab_eval, pd.DataFrame([row])], ignore_index=True)
+
+if model_type == "Regression":
+    row = {"model": model_name,
+           "hyperparameters": hyperParams,
+           #"fold": fold_number,
+           "rmse": mean_squared_error(y_test, y_pred),
+           "mae": mean_absolute_error(y_test, y_pred),
+           "maxe": max_error(y_test, y_pred)}
     tab_eval = pd.concat([tab_eval, pd.DataFrame([row])], ignore_index=True)
 
 with pd.option_context('display.max_rows', None,
